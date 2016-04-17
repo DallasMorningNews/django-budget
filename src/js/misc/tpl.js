d9a69f67c711d2268da403db14892f9aa8f3b0d0
Allan James Vestal <ajvestal@dallasnews.com>
2016-04-17 10:18:07 +0000

    Added 'numberWithCommas' filter to template-rendering module.

--- src/js/misc/tpl.js
+++ src/js/misc/tpl.js
@@ -1,19 +1,32 @@
-define(['templates', 'nunjucks'], function() {
+define(
+    [
+        'numeral',
+        'nunjucks',
+        'templates'
+    ],
+    function() {
+        'use strict';
 
-    'use strict';
+        var env = new nunjucks.Environment();
 
-    /**
-     * A helper to return Marionette-like template functions using pre-
-     * rendered Nunjucks templates
-     * @param string name - the name of the template (template.html would be
-     *   template)
-     * @return function - a template function that take the context and
-     *   returns HTML like Marionette expects
-     */
-    return function(name) {
-        return function(context) {
-            return nunjucks.render(name, context);
+        // Add 'numberWithCommas' filter.
+        env.addFilter('numberWithCommas', function(rawNumber) {
+            return numeral(rawNumber).format('0,0');
+        });
+
+        /**
+         * A helper to return Marionette-like template functions using pre-
+         * rendered Nunjucks templates
+         * @param string name - the name of the template (template.html would be
+         *   template)
+         * @return function - a template function that take the context and
+         *   returns HTML like Marionette expects
+         */
+        return function(name) {
+            return function(context) {
+                return env.render(name, context);
+            };
         };
-    };
 
-});
+    }
+);
