1ef11cc6187c375d9aa1be4a030d2653ae45ddb4
Allan James Vestal <ajvestal@dallasnews.com>
2016-04-19 23:24:50 +0000

    Added index-generation code to PackageCollection.

--- src/js/collections/packages.js
+++ src/js/collections/packages.js
@@ -1,12 +1,14 @@
 define(
     [
         'backbone',
+        'lunr',
         'underscore',
         'models/package',
         'misc/settings'
     ],
     function(
         Backbone,
+        lunr,
         _,
         Package,
         settings
@@ -26,6 +28,66 @@ define(
                 this.once('sync', function() {
                     this.queryFiltered = this.filter();
                 });
+            },
+
+            cleanSlug: function(rawSlug) {
+                return rawSlug
+                            .replace(/\./g, ' ')
+                            .replace(/-/g, ' ')
+                            .replace(/_/g, ' ');
+            },
+
+            combineAdditionalItemValues: function(model, field, fieldFormat) {
+                return _.chain(model.get('additionalContent'))
+                            .pluck(field)
+                            .reduce(
+                                function(memo, value) {
+                                    var finalValue = value;
+                                    if (!_.isUndefined(fieldFormat)) {
+                                        finalValue = fieldFormat(finalValue);
+                                    }
+
+                                    return memo + ' ' + finalValue;
+                                }
+                            ).value();
+            },
+
+            rebuildIndex: function() {
+                this.fullTextIndex = lunr(
+                    function() {
+                        this.field('relatedSlugs', {boost: 5});
+                        this.field('relatedSlugsCleaned', {boost: 10});
+                        this.field('relatedBudgetLines', {boost: 15});
+                        this.field('primarySlug', {boost: 20});
+                        this.field('primarySlugCleaned', {boost: 25});
+                        this.field('primaryBudgetLine', {boost: 30});
+                        this.ref('id');
+                    }
+                );
+
+                this.each(function(pkg) {
+                    this.fullTextIndex.add({
+                        id: pkg.get('id'),
+                        relatedSlugs: this.combineAdditionalItemValues(
+                            pkg,
+                            'slug'
+                        ),
+                        relatedSlugsCleaned: this.combineAdditionalItemValues(
+                            pkg,
+                            'slug',
+                            this.cleanSlug
+                        ),
+                        relatedBudgetLines: this.combineAdditionalItemValues(
+                            pkg,
+                            'budgetLine'
+                        ),
+                        primarySlug: pkg.get('primaryContent').slug,
+                        primarySlugCleaned: this.cleanSlug(
+                            pkg.get('primaryContent').slug
+                        ),
+                        primaryBudgetLine: pkg.get('primaryContent').budgetLine,
+                    });
+                }.bind(this));
             },
 
             filterAnd: function(queryTerms, extraContext) {
