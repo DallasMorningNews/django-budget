502b416cc0fb97d991fd25fa1cdea99b7968a460
Allan James Vestal <ajvestal@dallasnews.com>
2017-02-13 16:48:46 +0000

    Refactor budget-app layout views into ES6.

--- src/js/budget/layoutviews/root-view.js
+++ src/js/budget/layoutviews/root-view.js
@@ -1,132 +1,122 @@
-define([
-    'backbone',
-    'marionette',
-    'underscore',
-    'vex',
-    'common/tpl',
-    'budget/layoutviews/navbar',
-], function(
-    Backbone,
-    Mn,
-    _,
-    vex,
-    tpl,
-    NavbarView
-) {
-    return Mn.LayoutView.extend({
-        el: 'body',
-        template: tpl('root-view'),
-        regions: {
-            navbar: '#navigation',
-            mainContent: '#main-content',
-            snackbarHolder: '#snackbar-holder',
-        },
+import _ from 'underscore';
+import Backbone from 'backbone';
+import Mn from 'backbone.marionette';
+import vex from 'vex-js';
 
-        ui: {
-            modalHolder: '#modal-holder',
-        },
+import NavbarView from './navbar';
 
-        initialize: function() {
-            this._radio = Backbone.Wreqr.radio.channel('global');
+export default Mn.LayoutView.extend({
+    el: 'body',
+    template: 'budget/root-view',
+    regions: {
+        navbar: '#navigation',
+        mainContent: '#main-content',
+        snackbarHolder: '#snackbar-holder',
+    },
 
-            this.navbarView = new NavbarView({
+    ui: {
+        modalHolder: '#modal-holder',
+    },
+
+    initialize() {
+        this.radio = Backbone.Wreqr.radio.channel('global');
+
+        this.navbarView = new NavbarView({
+            currentUser: this.options.currentUser,
+        });
+
+        this.radio.commands.setHandler('switchMainView', (MainViewClass, boundData) => {
+            const classConstructor = {
                 currentUser: this.options.currentUser,
-            });
+                data: this.options.data,
+                state: this.options.state,
+                initFinishedCallback: (childView) => {
+                    childView.isAttached = true;  // eslint-disable-line no-param-reassign
+                    this.showChildView('mainContent', childView);
+                },
+            };
 
-            this._radio.commands.setHandler('switchMainView', function(MainViewClass, boundData) {
-                var classConstructor = {
-                    currentUser: this.options.currentUser,
-                    data: this.options.data,
-                    state: this.options.state,
-                    initFinishedCallback: function(childView) {
-                        childView.isAttached = true;  // eslint-disable-line no-param-reassign
-                        this.showChildView('mainContent', childView);
-                    }.bind(this),
-                };
-
-                if (typeof(boundData) !== 'undefined') {
-                    if (_.has(boundData, 'collection')) {
-                        classConstructor.collection = boundData.collection;
-                        delete boundData.collection;  // eslint-disable-line no-param-reassign
-                    }
-
-                    if (_.has(boundData, 'model')) {
-                        classConstructor.model = boundData.model;
-                        delete boundData.model;  // eslint-disable-line no-param-reassign
-                    }
-
-                    if (!_.isEmpty(boundData)) {
-                        classConstructor.boundData = boundData;
-                    }
+            if (typeof boundData !== 'undefined') {
+                if (_.has(boundData, 'collection')) {
+                    classConstructor.collection = boundData.collection;
+                    delete boundData.collection;  // eslint-disable-line no-param-reassign
                 }
 
-                this.mainView = new MainViewClass(classConstructor);
-            }, this);
-
-            this._radio.commands.setHandler('showModal', function(modalView) {
-                var vexConfig,
-                    viewConfig = modalView.options.modalConfig;
-
-                vexConfig = {
-                    appendLocation: this.ui.modalHolder,
-                    className: 'vex-theme-plain vex-theme-stack-demo',
-                    content: modalView.render().$el,
-                    showCloseButton: false,
-                    escapeButtonCloses: true,
-                    overlayClosesOnClick: true,
-                };
-
-                if (
-                    !_.isUndefined(viewConfig.contentClassName) &&
-                    !_.isNull(viewConfig.contentClassName)
-                ) {
-                    vexConfig.contentClassName = viewConfig.contentClassName;
+                if (_.has(boundData, 'model')) {
+                    classConstructor.model = boundData.model;
+                    delete boundData.model;  // eslint-disable-line no-param-reassign
                 }
 
-                if (
-                    (!_.isUndefined(viewConfig.escapeButtonCloses)) &&
-                    (!_.isNull(viewConfig.escapeButtonCloses)) &&
-                    (viewConfig.escapeButtonCloses === false)
-                ) {
-                    vexConfig.escapeButtonCloses = false;
+                if (!_.isEmpty(boundData)) {
+                    classConstructor.boundData = boundData;
                 }
+            }
 
-                if (
-                    (!_.isUndefined(viewConfig.overlayClosesOnClick)) &&
-                    (!_.isNull(viewConfig.overlayClosesOnClick)) &&
-                    (viewConfig.overlayClosesOnClick === false)
-                ) {
-                    vexConfig.overlayClosesOnClick = false;
-                }
+            this.mainView = new MainViewClass(classConstructor);
+        }, this);
 
-                if (
-                    (!_.isUndefined(viewConfig.showCloseButton)) &&
-                    (!_.isNull(viewConfig.showCloseButton)) &&
-                    (viewConfig.showCloseButton === true)
-                ) {
-                    vexConfig.showCloseButton = true;
-                }
+        this.radio.commands.setHandler('showModal', (modalView) => {
+            const viewConfig = modalView.options.modalConfig;
 
-                this.modal = vex.open(vexConfig);
-            }, this);
+            const vexConfig = {
+                appendLocation: `#${this.ui.modalHolder.attr('id')}`,
+                className: 'vex-theme-plain vex-theme-stack-demo',
+                unsafeContent: modalView.render().$el[0],
+                showCloseButton: false,
+                escapeButtonCloses: true,
+                overlayClosesOnClick: true,
+            };
 
-            this._radio.commands.setHandler('showSnackbar', function(snackbarView) {
-                this.snackbarView = snackbarView;
+            if (
+                !_.isUndefined(viewConfig.contentClassName) &&
+                !_.isNull(viewConfig.contentClassName)
+            ) {
+                vexConfig.contentClassName = viewConfig.contentClassName;
+            }
 
-                this.showChildView('snackbarHolder', this.snackbarView);
-            }, this);
+            if (
+                (!_.isUndefined(viewConfig.escapeButtonCloses)) &&
+                (!_.isNull(viewConfig.escapeButtonCloses)) &&
+                (viewConfig.escapeButtonCloses === false)
+            ) {
+                vexConfig.escapeButtonCloses = false;
+            }
 
-            this._radio.commands.setHandler('destroyModal', function() {
-                vex.close(this.modal.data().vex.id);
-            }, this);
+            if (
+                (!_.isUndefined(viewConfig.overlayClosesOnClick)) &&
+                (!_.isNull(viewConfig.overlayClosesOnClick)) &&
+                (viewConfig.overlayClosesOnClick === false)
+            ) {
+                vexConfig.overlayClosesOnClick = false;
+            }
 
-            this._radio.commands.setHandler('clearRegion', function(regionSlug) {
-                this[regionSlug].reset();
-            }, this);
-        },
+            if (
+                (!_.isUndefined(viewConfig.showCloseButton)) &&
+                (!_.isNull(viewConfig.showCloseButton)) &&
+                (viewConfig.showCloseButton === true)
+            ) {
+                vexConfig.showCloseButton = true;
+            }
 
-        onRender: function() {
-            this.showChildView('navbar', this.navbarView);
-        },
-    });
+            this.modal = vex.open(vexConfig);
+        }, this);
+
+        this.radio.commands.setHandler('showSnackbar', (snackbarView) => {
+            this.snackbarView = snackbarView;
+
+            this.showChildView('snackbarHolder', this.snackbarView);
+        }, this);
+
+        this.radio.commands.setHandler('destroyModal', () => {
+            this.modal.close();
+        }, this);
+
+        this.radio.commands.setHandler('clearRegion', (regionSlug) => {
+            this[regionSlug].reset();
+        }, this);
+    },
+
+    onRender() {
+        this.showChildView('navbar', this.navbarView);
+    },
 });
