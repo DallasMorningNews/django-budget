ec0fcba3d3b5dde8a857ccb1711b3550bd21edbb
Allan James Vestal <allanjamesvestal@gmail.com>
2017-09-07 18:13:37 -0500

    Revised spacing for misc-folder budget app files.

--- budget/staticapp/src/js/budget/misc/controller.js
+++ budget/staticapp/src/js/budget/misc/controller.js
@@ -10,88 +10,88 @@ import WebSearchList from '../compositeviews/search-list/web';
 const radio = Backbone.Wreqr.radio.channel('global');
 
 export default {
-    home(querystring) {
+  home(querystring) {
+    radio.commands.execute(
+      'setState',
+      'meta',
+      'listViewType',
+      'listPage'  // eslint-disable-line comma-dangle
+    );
+
+    radio.commands.execute(
+      'switchMainView',
+      WebSearchList,
+      { querystring }  // eslint-disable-line comma-dangle
+    );
+  },
+  printList(querystring) {
+    radio.commands.execute('setState', 'meta', 'listViewType', 'printListPage');
+
+    radio.commands.execute(
+        'switchMainView',
+        PrintSearchList,
+        { querystring }  // eslint-disable-line comma-dangle
+    );
+  },
+  edit(packageID) {
+    const packageOpts = (_.isUndefined(packageID)) ? {} : {
+      id: parseInt(packageID, 10),
+    };
+    const packageToEdit = new Package(packageOpts);
+
+    if (_.isUndefined(packageID)) {
+      // Instantiate an item collection associated with this package, and
+      // retrieve its starting values from the API.
+      packageToEdit.loadInitial().done(() => {
         radio.commands.execute(
-            'setState',
-            'meta',
-            'listViewType',
-            'listPage'
+          'switchMainView',
+          PackageEditView,
+          { model: packageToEdit, isEmpty: true }  // eslint-disable-line comma-dangle
         );
-
+      });
+    } else {
+      packageToEdit.fetch({
+        xhrFields: {
+          withCredentials: true,
+        },
+      }).done(() => {
         radio.commands.execute(
-            'switchMainView',
-            WebSearchList,
-            { querystring }
+          'switchMainView',
+          PackageEditView,
+          { model: packageToEdit }  // eslint-disable-line comma-dangle
         );
-    },
-    printList(querystring) {
-        radio.commands.execute('setState', 'meta', 'listViewType', 'printListPage');
+      }).fail((stage, response) => {
+        if (stage === 'package') {
+          if (response.status === 404) {
+            // Redirect to the home page.
+            radio.commands.execute('navigate', '', { trigger: true });
 
-        radio.commands.execute(
-            'switchMainView',
-            PrintSearchList,
-            { querystring }
-        );
-    },
-    edit(packageID) {
-        const packageOpts = (_.isUndefined(packageID)) ? {} : {
-            id: parseInt(packageID, 10),
-        };
-        const packageToEdit = new Package(packageOpts);
+            // Display snackbar:
+            radio.commands.execute(
+              'showSnackbar',
+              new SnackbarView({
+                snackbarClass: 'failure',
+                text: 'Could not find that item.',
+              })  // eslint-disable-line comma-dangle
+            );
+          } else {
+            // Redirect to the home page.
+            radio.commands.execute('navigate', '', { trigger: true });
 
-        if (_.isUndefined(packageID)) {
-            // Instantiate an item collection associated with this package, and
-            // retrieve its starting values from the API.
-            packageToEdit.loadInitial().done(() => {
-                radio.commands.execute(
-                    'switchMainView',
-                    PackageEditView,
-                    { model: packageToEdit, isEmpty: true }
-                );
-            });
-        } else {
-            packageToEdit.fetch({
-                xhrFields: {
-                    withCredentials: true,
-                },
-            }).done(() => {
-                radio.commands.execute(
-                    'switchMainView',
-                    PackageEditView,
-                    { model: packageToEdit }
-                );
-            }).fail((stage, response) => {
-                if (stage === 'package') {
-                    if (response.status === 404) {
-                        // Redirect to the home page.
-                        radio.commands.execute('navigate', '', { trigger: true });
-
-                        // Display snackbar:
-                        radio.commands.execute(
-                            'showSnackbar',
-                            new SnackbarView({
-                                snackbarClass: 'failure',
-                                text: 'Could not find that item.',
-                            })
-                        );
-                    } else {
-                        // Redirect to the home page.
-                        radio.commands.execute('navigate', '', { trigger: true });
-
-                        // Display snackbar:
-                        radio.commands.execute(
-                            'showSnackbar',
-                            new SnackbarView({
-                                snackbarClass: 'failure',
-                                text: 'Could not load that item.',
-                            })
-                        );
-                    }
-                }
-            });
+            // Display snackbar:
+            radio.commands.execute(
+              'showSnackbar',
+              new SnackbarView({
+                snackbarClass: 'failure',
+                text: 'Could not load that item.',
+              })  // eslint-disable-line comma-dangle
+            );
+          }
         }
-    },
-    fourohfour() {
-        console.log('404.');  // eslint-disable-line no-console
-    },
+      });
+    }
+  },
+  fourohfour() {
+    console.log('404.');  // eslint-disable-line no-console
+  },
 };
