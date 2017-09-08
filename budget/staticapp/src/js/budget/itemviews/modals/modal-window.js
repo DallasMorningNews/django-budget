678f18ba36a83d1be0404a4c83de43d9eb9e61ee
Allan James Vestal <allanjamesvestal@gmail.com>
2017-09-08 00:49:01 -0500

    Style fixes for budget static app.

--- budget/staticapp/src/js/budget/itemviews/modals/modal-window.js
+++ budget/staticapp/src/js/budget/itemviews/modals/modal-window.js
@@ -4,72 +4,68 @@ import Mn from 'backbone.marionette';
 import 'selectize';
 
 export default Mn.ItemView.extend({
-    template: 'budget/modal-base',
+  template: 'budget/modal-base',
 
-    serializeData() {
-        return this.options.modalConfig;
-    },
+  serializeData() {
+    return this.options.modalConfig;
+  },
 
-    ui: {
-        // searchBox: '#package-search-box'
-        button: '.button-holder .material-button',
-    },
+  ui: {
+    // searchBox: '#package-search-box'
+    button: '.button-holder .material-button',
+  },
 
-    events: {
-        'mousedown @ui.button': 'addButtonClickedClass',
-        'click @ui.button': 'onButtonClick',
-    },
+  events: {
+    'mousedown @ui.button': 'addButtonClickedClass',
+    'click @ui.button': 'onButtonClick',
+  },
 
-    initialize(options) {
-        if (_.has(options, 'model')) {
-            this.model = this.options.model;
-        }
-    },
+  initialize(options) {
+    if (_.has(options, 'model')) {
+      this.model = this.options.model;
+    }
+  },
 
-    // className: 'center-content',
-    // regions: {
-    //     filters: "#filters",
-    //     packages: "#packages"
-    // },
+  // className: 'center-content',
+  // regions: {
+  //   filters: "#filters",
+  //   packages: "#packages"
+  // },
 
-    addButtonClickedClass(event) {
-        const thisEl = jQuery(event.currentTarget);
+  addButtonClickedClass(event) {
+    const thisEl = jQuery(event.currentTarget);
 
-        thisEl.addClass('hover').addClass('active-state');
-        thisEl.removeClass('click-init');
+    thisEl.addClass('hover').addClass('active-state');
+    thisEl.removeClass('click-init');
 
-        setTimeout(
-            () => { thisEl.removeClass('hover').removeClass('active-state'); },
-            1000
-        );
+    setTimeout(() => {
+      thisEl.removeClass('hover').removeClass('active-state');
+    }, 1000);
 
-        setTimeout(
-            () => { thisEl.addClass('click-init'); },
-            2000
-        );
-    },
+    setTimeout(() => {
+      thisEl.addClass('click-init');
+    }, 2000);
+  },
 
-    onButtonClick(event) {
-        const thisEl = jQuery(event.currentTarget);
+  onButtonClick(event) {
+    const thisEl = jQuery(event.currentTarget);
 
-        const modalConfig = _.findWhere(
-            this.options.modalConfig.buttons,
-            {
-                buttonID: thisEl.attr('id'),
-            }
-        );
+    const modalConfig = _.findWhere(
+      this.options.modalConfig.buttons,
+      { buttonID: thisEl.attr('id') }  // eslint-disable-line comma-dangle
+    );
 
-        modalConfig.clickCallback(this);
-    },
+    modalConfig.clickCallback(this);
+  },
 
-    onRender() {
-        if (_.has(this.options, 'renderCallback')) {
-            this.options.renderCallback(this);
-        }
-    },
+  onRender() {
+    if (_.has(this.options, 'renderCallback')) {
+      this.options.renderCallback(this);
+    }
+  },
 
-    // initialize() {
-    //     this.packageFilterView = new PackageFilterView({});
-    //     this.packageCollectionView = new ({});
-    // },
+  // initialize() {
+  //   this.packageFilterView = new PackageFilterView({});
+  //   this.packageCollectionView = new ({});
+  // },
 });
--- budget/staticapp/src/js/budget/models/staffer.js
+++ budget/staticapp/src/js/budget/models/staffer.js
@@ -1,4 +1,3 @@
-// import Backbone from 'backbone';
 import CSRFAwareModel from '../../common/csrf-aware-model';
 
 export default CSRFAwareModel.extend({});
