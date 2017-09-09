748c66c52464fe678c530fb745bc25161212e7f5
Allan James Vestal <allanjamesvestal@gmail.com>
2017-09-08 20:16:21 -0500

    Budget app: pass image path for The Daily through to list-view template.

--- budget/staticapp/src/js/budget/itemviews/list-components/daily-title.js
+++ budget/staticapp/src/js/budget/itemviews/list-components/daily-title.js
@@ -1,5 +1,18 @@
+import Backbone from 'backbone';
 import Mn from 'backbone.marionette';
 
 export default Mn.ItemView.extend({
-    template: 'budget/list-components-print-daily-title',
+  template: 'budget/list-components-print-daily-title',
+
+  initialize() {
+    this.radio = Backbone.Wreqr.radio.channel('global');
+  },
+
+  serializeData() {
+    const brandingOpts = this.radio.reqres.request('getSetting', 'branding');
+
+    return {
+      printLogoURL: brandingOpts.printLogoURL,
+    };
+  },
 });
