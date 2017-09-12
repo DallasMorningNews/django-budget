85624e49973d16babc3d17dc84b921d004dc22cc
Allan James Vestal <allanjamesvestal@gmail.com>
2017-09-11 19:28:05 -0500

    Add value of 'settings.BUDGET_PRINT_SLUG_NAME' to Budget config JSON.

--- budget/views.py
+++ budget/views.py
@@ -169,6 +169,7 @@ class ConfigView(View):
             'externalURLs': json.dumps(
                 getattr(settings, 'BUDGET_EXTERNAL_URLS', {})
             ),
+            'printSlugName': getattr(settings, 'BUDGET_PRINT_SLUG_NAME', None),
             'showHeadlines': getattr(settings, 'BUDGET_SHOW_HEADLINES', False),
             'rootURL': root_url
         })
