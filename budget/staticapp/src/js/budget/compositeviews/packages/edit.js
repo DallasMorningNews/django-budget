7a7810111a2e1203b7de606113552d6552fd3a06
Allan James Vestal <allanjamesvestal@gmail.com>
2017-12-03 14:29:55 -0600

    Modify package-edit ItemView to pass renamed context items to ContentPlacementForm (will be reversed when the edit view's content-placement variable names change to reflect the refactor).

--- budget/staticapp/src/js/budget/compositeviews/packages/edit.js
+++ budget/staticapp/src/js/budget/compositeviews/packages/edit.js
@@ -447,7 +447,12 @@ export default Mn.CompositeView.extend({
 
     const contentPlacementForm = new ContentPlacementForm({
       model,
-      extraContext: this,
+      // extraContext: this,
+      extraContext: {
+        destinationModels: this.options.data.printPublications,
+        destinations: this.printPlacementChoices,
+        destinationPlacements: this.printPublicationSections,
+      },
       callbacks: {
         delete: () => {
           this.radio.commands.execute('destroyModal');
