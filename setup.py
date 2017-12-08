db0acddac16a2d89830be8946df82647c177706d
Allan James Vestal <allanjamesvestal@gmail.com>
2017-12-08 15:56:24 -0600

    Prepare v0.8.2 release.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 REPO_URL = 'https://github.com/DallasMorningNews/django-budget/'
 
-PYPI_VERSION = '0.8.1'
+PYPI_VERSION = '0.8.2'
 
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
