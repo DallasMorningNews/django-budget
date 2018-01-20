7811dda0f45ee69b5cecd6899a868ade5b4b3179
Allan James Vestal <allanjamesvestal@gmail.com>
2018-01-20 16:38:58 -0600

    Bumped django-editorial-staff requirement to v0.7.0. Oops.

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     install_requires=[
         'Django>=1.9.0,<2.0',
         'django-admin-sortable2~=0.6.16',
-        'django-editorial-staff>=0.5.2',
+        'django-editorial-staff>=0.7.0',
         'django-filter~=1.0.0',
         'djangorestframework~=3.6.0',
         'djangorestframework-camel-case~=0.2.0',
